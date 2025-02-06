d0e307de3d605097f02b29fd40bcc0be1c6ba0d9
Kakimbek Nyssanov <kakimbekn@gmail.com>
2025-02-06 18:28:50 +0500

    first commit

---
+++ About me/About me/About_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_meApp.swift
+//  About me
+//
+//  Created by kakim nyssanov on 06.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct About_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ About me/About me/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  About me
+//
+//  Created by kakim nyssanov on 06.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
